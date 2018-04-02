86e06bc1fe2914a5262a938fda606dc165612f3b
Jan-André Zinser <zinserjan@gmail.com>
2018-04-02 16:04:01 +0200

    consistent logs

--- src/methods/Spectre.js
+++ src/methods/Spectre.js
@@ -24,12 +24,12 @@ export default class Spectre extends BaseCompare {
 
   async onPrepare() {
     const creationOptions = `Api-Url: ${this.spectreURL}, Project: ${this.project}, Suite: ${this.suite}`;
-    log(`${creationOptions} - creating testrun`);
+    log(`${creationOptions} - Creating testrun`);
     const result = await this.spectreClient.createTestrun(this.project, this.suite);
-    log(`${creationOptions} - testrun created - Run-Id: #${result.id}`);
+    log(`${creationOptions} - Testrun created - Run-Id: #${result.id}`);
     try{
       await fs.writeJson(pathToRunIDJson, result);
-      log(`Saved Run-Id #${result.id} to ${pathToRunIDJson}`);
+      log(`${creationOptions} - Saved Run-Id #${result.id} to ${pathToRunIDJson}`);
     }
     catch (e){
       throw new Error(`Unable to write file to ${pathToRunIDJson}`);
@@ -42,10 +42,10 @@ export default class Spectre extends BaseCompare {
     const fuzzLevel = `${_.get(context, 'options.fuzzLevel', this.fuzzLevel)}%`;
 
     const uploadName = `Test: ${spectreUploadOptions.testName}, Browser: ${spectreUploadOptions.browser}, Size: ${spectreUploadOptions.size}, Run-Id:  #${testrunID}`;
-    log(`${uploadName} - starting upload`);
+    log(`${uploadName} - Starting upload`);
 
     const result = await this.spectreClient.submitScreenshot(spectreUploadOptions.testName, spectreUploadOptions.browser, spectreUploadOptions.size, base64Screenshot, testrunID, '', context.meta.url, fuzzLevel);
-    log(`${uploadName} - upload successful`);
+    log(`${uploadName} - Upload successful`);
 
     if (result.pass) {
       log(`${uploadName} - Image is within tolerance or the same`);
