33fba0fb3d68452a8503932f3711f4501e335cc7
Jan-André Zinser <zinserjan@gmail.com>
2018-04-04 20:15:59 +0200

    generate unique runtime configs per webdriverio test runner

--- src/methods/BaseCompare.js
+++ src/methods/BaseCompare.js
@@ -1,4 +1,13 @@
+import fs from 'fs-extra';
+import path from 'path';
+
+const runtimeConfigPath = __dirname;
+
 export default class BaseCompare {
+
+  constructor() {
+    this.configs = new Map();
+  }
 
   /**
    * Gets executed once before all workers get launched.
@@ -50,7 +59,66 @@ export default class BaseCompare {
     return Promise.resolve();
   }
 
+  /**
+   * Prepare runtime config for worker process
+   */
+  async saveRuntimeConfig(name, config) {
+    const file = this.getRuntimeConfigFileName(name);
+    await fs.writeJson(file, config, {
+      spaces: 2
+    });
+    this.configs.set(name, config);
+  }
 
+   /**
+   * Read prepared runtime config from launcher process
+   */
+  async getRuntimeConfig(name) {
+    // try to read from cache first
+    if (this.configs.has(name)) {
+      return this.configs.get(name);
+    }
+    // otherwise read from fs
+    const file = this.getRuntimeConfigFileName(name);
+    const config = await fs.readJson(file);
+    // and cache the result
+    this.configs.set(name, config);
+    return config;
+  }
+
+   /**
+   * Remove runtime config
+   */
+  async removeRuntimeConfig(name) {
+    // delete from fs
+    const file = this.getRuntimeConfigFileName(name);
+    await fs.remove(file);
+    // delete from cache
+    this.configs.delete(name);
+  }
+
+  /**
+   * Removes all created runtime configs
+   */
+  async cleanUpRuntimeConfigs() {
+    // clean up all saved config files
+    const names = [...this.configs.keys()];
+    await Promise.all(names.map((n) => this.removeRuntimeConfig(n)));
+  }
+
+  /**
+   * Builds a non-conflicting file name for this webdriverio run
+   */
+  getRuntimeConfigFileName(name) {
+    // launcher and runner gets the same arguments, so let's use them to build a hash to determine different calls
+    const hash = require("crypto").createHash('md5').update(process.argv.slice(2).join("")).digest('hex').substring(0, 4);
+    // try to use process id to generate a unique file name for each webdriverio instance
+    const runner = global.browser != null;
+    const pid = !process.hasOwnProperty("ppid") ? null : (runner ? process.ppid : process.pid);
+    // generate file name suffix
+    const suffix = [hash, pid].filter(Boolean).join("-");
+    return path.join(runtimeConfigPath, `${name}-${suffix}.json`);
+  }
 
   createResultReport(misMatchPercentage, isWithinMisMatchTolerance, isSameDimensions) {
     return {
